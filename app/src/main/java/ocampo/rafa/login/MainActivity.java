80f9b2404177792dca0ba7457e6429d5b932d81b
rafa <rafaocsa89@gmail.com>
2019-08-28 17:52:25 -0500

    Initial commit

---
+++ app/src/main/java/ocampo/rafa/login/MainActivity.java
@@ -0,0 +1,14 @@
+package ocampo.rafa.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
